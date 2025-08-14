9d04b90e823b63e1ef7546c6c00553968e6c9d53
pay-pies <23pxie@kendrick.reading.sch.uk>
2025-08-14 21:44:00 +0100

    added files to git

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,2 +1,17 @@
-import {Account, Client, Databases} from 'react-native-appwrite'
+import { Account, Client, Databases } from 'react-native-appwrite';
+
 const client = new Client()
+    .setEndpoint("https://fra.cloud.appwrite.io/v1")
+    .setProject("689e405900149758a975")
+    .setPlatform("com.paypies.habittracker");
+
+const account = new Account(client);
+
+// Test request — get current account (will fail if not logged in, but still connects)
+account.get()
+    .then(response => {
+        console.log("Connected! Account info:", response);
+    })
+    .catch(err => {
+        console.log("Connected but not logged in:", err.message);
+    });
