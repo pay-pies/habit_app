8e7ac30cca1ee43fb843818afbf0da631dc8de30
pay-pies <23pxie@kendrick.reading.sch.uk>
2025-08-14 12:51:41 +0100

    added files to git

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,27 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet} from "react-native";
 import {Link} from "expo-router";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+      style={styles.view}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href="/login" style={{}}>Login Page</Link>
+      <Link href="/login" style= {styles.navButton}>Login Page</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  view:{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  navButton: {
+      width: 100, 
+      height: 20, 
+      backgroundColor: 'coral', 
+      borderRadius: 8,
+      textAlign: 'center',
+    }
+})
