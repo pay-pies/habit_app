f90c7651678e8f401c489a78d0fe16c6e49a96d9
pay-pies <23pxie@kendrick.reading.sch.uk>
2025-08-16 19:11:05 +0100

    fixed problems with appwrite 'rate limit'

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
 import { Text, View, StyleSheet} from "react-native";
-import {Link} from "expo-router";
+import {Button} from "react-native-paper";
+import { useAuth } from "@/lib/auth-context";
 
 export default function Index() {
+  const {signOut} = useAuth();
   return (
     <View
       style={styles.view}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button mode="text" onPress={signOut} icon="logout">Sign Out</Button>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,22 @@
-import { Stack, useRouter } from "expo-router";
-import { Route } from "expo-router/build/Route";
-import { useEffect, useState } from "react";
-import { AuthProvider } from '../lib/auth-context';
+import { Stack, useRouter, useSegments } from "expo-router";
+import { useEffect} from "react";
+import { AuthProvider, useAuth } from '../lib/auth-context';
 
 function RouteGuard({children}: {children: React.ReactNode}) {
   const router = useRouter();
-  const [mounted, setMounted] = useState(false);
-  const isAuth = false; // Replace with actual authentication check
-
-    useEffect(() => {
-    setMounted(true);
-  }, []);
+  const {user, isLoadingUser} = useAuth();  
+  const segments = useSegments();
 
   useEffect(() => {
-    if (mounted && !isAuth) {
+    if (isLoadingUser) return;
+    const inAuthGroup = segments[0] === "auth";
+
+    if (!user && !inAuthGroup && !isLoadingUser) {
       router.replace("/auth");
+    } else if (user && inAuthGroup && !isLoadingUser) {
+      router.replace("/");
     }
-  }, [mounted, isAuth]);
+  }, [user, segments, isLoadingUser, router]);
   
 
   return <>{children}</>;
@@ -28,6 +28,7 @@ export default function RootLayout() {
       <RouteGuard>
         <Stack>
           <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+          <Stack.Screen name="auth" options={{ headerShown: false }} />
         </Stack>
       </RouteGuard>
     </AuthProvider>
