8584f7015309e8bfaa41492780deca37ba7882d5
pay-pies <23pxie@kendrick.reading.sch.uk>
2025-08-16 17:29:02 +0100

    modified auth-context.tsx

---
+++ lib/auth-context.tsx
@@ -0,0 +1,47 @@
+import {createContext} from "react";
+import {Models, ID, Account} from "react-native-appwrite";
+import { account } from "../lib/appwrite"; 
+
+
+type AuthContextType = {
+    // user: Models.User<Models.Preferences> | null;
+    signUp: (email: string, password: string) => Promise<string | null>;
+    signIn: (email: string, password: string) => Promise<string | null>;
+}
+const AuthContext = createContext<AuthContextType | undefined>(undefined)
+
+export function AuthProvider({children}: {children: React.ReactNode}) {
+    
+    const signUp = async (email: string, password: string) => {
+        try{
+            await account.create(ID.unique(), email, password)
+            await signIn(email, password);
+            return null;
+        } catch (error) {
+            if (error instanceof Error) {
+                return error.message;
+            }
+            return "An error occurred during sign up.";
+        }
+    };
+    const signIn = async (email: string, password: string) => {
+        try{
+            await account.createEmailPasswordSession(email, password)
+            return null;
+        } catch (error) {
+            if (error instanceof Error) {
+                return error.message;
+            }}
+            return "An error occurred during sign in.";
+    };
+    return( 
+    <AuthContext.Provider value={{signUp, signIn}}>
+        {children}
+    </AuthContext.Provider>
+    );
+
+}
+
+export function useAuth() {
+
+}
