52e66dbed591988dbe3ceef78fc7be0d3f51d7ac
pay-pies <23pxie@kendrick.reading.sch.uk>
2025-08-18 13:28:21 +0100

    added functionality to add-habit page

--- app/(tabs)/add-habit.tsx
+++ app/(tabs)/add-habit.tsx
@@ -1,25 +1,10 @@
 import {View, StyleSheet} from 'react-native';
-import {TextInput, SegmentedButtons, Button} from "react-native-paper";
-
-const FREQUENCIES = ["daily", "weekly", "monthly"];
-
-export default function AddHabitScreen() {
-    return(
-        <View style={styles.container}>
-            <TextInput label="Title" mode="outlined" style={styles.input}></TextInput>
-            <TextInput label="Description" mode="outlined" style={styles.input}></TextInput>
-            <View style={styles.frequencyContainer}>
-                <SegmentedButtons buttons={FREQUENCIES.map((freq) => ({
-                    value: freq,
-                    label: freq.charAt(0).toUpperCase() + freq.slice(1),
-                }))}/>
-            </View>
-            <Button mode="contained">
-                Add Habit
-            </Button>
-        </View>
-    );
-}
+import {useState} from 'react';
+import {useAuth} from '../../lib/auth-context';
+import {TextInput, SegmentedButtons, Button, useTheme, Text} from "react-native-paper";
+import {databases, DATABASE_ID, HABIT_COLLECTION_ID} from '../../lib/appwrite';
+import {ID} from 'react-native-appwrite';
+import {useRouter} from 'expo-router';
 
 const styles = StyleSheet.create({
     container:{
@@ -35,3 +20,77 @@ const styles = StyleSheet.create({
         marginBottom: 24,
     },
 });
+
+const FREQUENCIES = ["daily", "weekly", "monthly"];
+type Frequency = (typeof FREQUENCIES)[number];
+export default function AddHabitScreen() {
+    const [title, setTitle] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+    const [frequency, setFrequency] = useState<Frequency>("daily");
+    const {user} = useAuth();
+    const router = useRouter();
+    const [error, setError] = useState<string>("");
+    const theme = useTheme();
+    const handleSubmit = async () => {
+        if (!user) return;
+        try{
+        await databases.createDocument(
+            DATABASE_ID, 
+            HABIT_COLLECTION_ID, 
+            ID.unique(),
+            {
+                user_id: user.$id,
+                title,
+                description,
+                frequency,
+                streak_count: 0,
+                last_completed: new Date().toISOString(),
+                created_at: new Date().toISOString(),
+            }
+        );
+        router.back();
+    } catch (error) {
+        if (error instanceof Error) {
+            setError(error.message);
+            return;
+        } 
+        setError("An error occurred while adding the habit.");
+    }
+    
+};
+
+
+
+return(
+    <View style={styles.container}>
+        <TextInput 
+            label="Title" 
+            mode="outlined" 
+            style={styles.input} 
+            onChangeText={setTitle}
+            />
+        <TextInput 
+            label="Description" 
+            mode="outlined" 
+            style={styles.input}
+            onChangeText={setDescription}/>
+        <View style={styles.frequencyContainer}>
+            <SegmentedButtons 
+                value={frequency}
+                onValueChange={(value) => setFrequency(value as Frequency)}
+                buttons={FREQUENCIES.map((freq) => ({
+                    value: freq,
+                    label: freq.charAt(0).toUpperCase() + freq.slice(1),
+            }))}
+        />
+        </View>
+        <Button mode="contained" onPress={handleSubmit}disabled={!title || !description}>
+            Add Habit
+        </Button>
+        {error &&
+                <Text style={{color: theme.colors.error}}>{error}</Text>
+                
+            }
+        </View>
+    );
+}
