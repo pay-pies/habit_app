5262975e3615204c6f061aa59c5a5c3d1d1a24ea
pay-pies <23pxie@kendrick.reading.sch.uk>
2025-10-08 19:59:29 +0100

    removed 'process.env' bits

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,24 +1,38 @@
+import Constants from 'expo-constants';
 import 'react-native-url-polyfill/auto';
 import { Account, Client, Databases } from 'react-native-appwrite';
 
+const {
+  EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  EXPO_PUBLIC_DB_ID,
+  EXPO_PUBLIC_HABIT_COLLECTION_ID,
+  EXPO_PUBLIC_COMPLETIONS_COLLECTION_ID,
+} = Constants.expoConfig?.extra || {};
+
 export const client = new Client()
-    .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!)
-    .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
-    .setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM!);
+    .setEndpoint(EXPO_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(EXPO_PUBLIC_APPWRITE_PROJECT_ID)
+    // .setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM!);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
 
-export const DATABASE_ID = process.env.EXPO_PUBLIC_DB_ID!;
-export const HABIT_COLLECTION_ID = 
-    process.env.EXPO_PUBLIC_HABIT_COLLECTION_ID!;
-    export interface RealTimeResponse {
-        events: string[];
-        payload: any;
-    }
-export const COMPLETIONS_COLLECTION_ID = 
-    process.env.EXPO_PUBLIC_COMPLETIONS_COLLECTION_ID!;
-    export interface RealTimeResponse {
-        events: string[];
-        payload: any;
-    }
+export const DATABASE_ID = EXPO_PUBLIC_DB_ID;
+export const HABIT_COLLECTION_ID = EXPO_PUBLIC_HABIT_COLLECTION_ID;
+    // process.env.EXPO_PUBLIC_HABIT_COLLECTION_ID!;
+    // export interface RealTimeResponse {
+    //     events: string[];
+    //     payload: any;
+    // }
+export const COMPLETIONS_COLLECTION_ID = EXPO_PUBLIC_COMPLETIONS_COLLECTION_ID;
+    // process.env.EXPO_PUBLIC_COMPLETIONS_COLLECTION_ID!;
+    // export interface RealTimeResponse {
+    //     events: string[];
+    //     payload: any;
+    // }
+
+export interface RealTimeResponse {
+  events: string[];
+  payload: any;
+}
